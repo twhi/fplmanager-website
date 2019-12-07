ca744ddbadd7c5e8ab3aa1701f77500c01c91475
Tom <twhitehead.1991@gmail.com>
2019-12-07 15:46:23 +0000

    first commit

--- FPLManager/settings_prod.py
+++ FPLManager/settings_prod.py
@@ -6,19 +6,19 @@ from .settings_common import *  # IMPORT COMMON SETTINGS
 
 ALLOWED_HOSTS = []  # prod
 
-# AWS S3 bucket credentials
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_BUCKET_REGION')
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_BUCKET_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_BUCKET_SECRET_ACCESS_KEY') 
+# # AWS S3 bucket credentials
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
+# AWS_S3_REGION_NAME = os.environ.get('AWS_BUCKET_REGION')
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_BUCKET_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_BUCKET_SECRET_ACCESS_KEY') 
 
-# AWS S3 bucket settings
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_DEFAULT_ACL = None
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+# # AWS S3 bucket settings
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_DEFAULT_ACL = None
+# STATICFILES_LOCATION = 'static'
+# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+# MEDIAFILES_LOCATION = 'media'
+# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 # Production Database
 DATABASES = {
