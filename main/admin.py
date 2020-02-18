49a9c2ebf8ba60aea865dcdabaae24492a8ad8f5
Tom <twhitehead.1991@gmail.com>
2020-02-18 18:25:30 +0000

    Output and delete model objects from Django Admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
+import csv
+
 from .models import Player, Team, Usage
+
 
 class NoLoggingMixin:
     def log_addition(self, *args):
@@ -11,14 +15,50 @@ class NoLoggingMixin:
     def log_deletion(self, *args):
         return
 
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    def export_delete_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+            obj.delete()
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+    export_delete_as_csv.short_description = "Export and Delete Selected"
+
 @admin.register(Player)
-class PlayerAdmin(NoLoggingMixin, admin.ModelAdmin):
+class PlayerAdmin(NoLoggingMixin, admin.ModelAdmin, ExportCsvMixin):
     readonly_fields = ('updated',)
+    actions = ['export_as_csv']
 
 @admin.register(Team)
-class TeamAdmin(NoLoggingMixin, admin.ModelAdmin):
+class TeamAdmin(NoLoggingMixin, admin.ModelAdmin, ExportCsvMixin):
     readonly_fields = ('updated',)
+    actions = ['export_as_csv']
 
 @admin.register(Usage)
-class UsageAdmin(NoLoggingMixin, admin.ModelAdmin):
+class UsageAdmin(NoLoggingMixin, admin.ModelAdmin, ExportCsvMixin):
     readonly_fields = ('updated',)
+    actions = ['export_as_csv', 'export_delete_as_csv']
