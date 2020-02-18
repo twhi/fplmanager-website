8b0ae4e59bd32db75e48aec92b5e4b0b90b8d265
Tom <twhitehead.1991@gmail.com>
2020-02-18 18:11:47 +0000

    Removed logging in Django Admin

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 from .models import Player, Team, Usage
 
+class NoLoggingMixin:
+    def log_addition(self, *args):
+        return
+    
+    def log_change(self, *args):
+        return
+
+    def log_deletion(self, *args):
+        return
+
 @admin.register(Player)
-class PlayerAdmin(admin.ModelAdmin):
+class PlayerAdmin(NoLoggingMixin, admin.ModelAdmin):
     readonly_fields = ('updated',)
 
 @admin.register(Team)
-class TeamAdmin(admin.ModelAdmin):
+class TeamAdmin(NoLoggingMixin, admin.ModelAdmin):
     readonly_fields = ('updated',)
 
 @admin.register(Usage)
-class UsageAdmin(admin.ModelAdmin):
+class UsageAdmin(NoLoggingMixin, admin.ModelAdmin):
     readonly_fields = ('updated',)
